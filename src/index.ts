dc9f68b34e00e286d457732f48b2a3ad9bbc2d39
Matheus Melo <matheuscabal821@gmail.com>
2023-01-05 12:03:21 -0300

    📦 NEW: Error handler added to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import express from "express";
-import cors from "cors";
-import "express-async-errors";
-import routes from "./routes/index";
+import express from 'express';
+import cors from 'cors';
+import 'express-async-errors';
+import routes from './routes/index';
+import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
+
 app.use(cors(), express.json());
 
 app.use(routes);
 
-app.get("/test", (req, res) => {
-  res.status(200).send("Hello world");
+app.use(errorHandler);
+
+app.get('/test', (req, res) => {
+  res.status(200).send('Hello world');
 });
 
 export default app;
