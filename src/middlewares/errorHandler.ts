ad0fbd0090cf77969090357705edcc25497c78dd
Matheus Melo <matheuscabal821@gmail.com>
2023-01-11 16:15:48 -0300

    👌 IMPROVE:

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -13,14 +13,13 @@ const ERRORS: any = {
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function errorHandler(error: error, req: Request, res: Response, next: NextFunction) {
-  let statusCode: number = ERRORS[error.type];
-
+export async function errorHandler(err: error, req: Request, res: Response, next: NextFunction) {
+  let statusCode: number = ERRORS[err.type];
   if (!statusCode) {
-    console.log(error, Date.now());
     statusCode = 500;
+    console.log({ ...err, time: new Date()});
     return res.sendStatus(statusCode); // internal server error
   }
 
-  return res.status(statusCode).send(error.message);
+  return res.status(statusCode).send(err);
 }
