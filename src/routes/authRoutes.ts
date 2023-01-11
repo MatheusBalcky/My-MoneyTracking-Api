7261d64a77a052dfd67b1414da047f4f501fbb6b
Matheus Melo <matheuscabal821@gmail.com>
2023-01-11 16:14:39 -0300

    📦 NEW: middleware to validate schemas by joi added

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,12 @@
-import { Router } from "express";
-import * as authControlls from "../controllers/authControllers";
+import { Router } from 'express';
+import validateSchemas from '../middlewares/validateSchemas';
+import * as authSchemas from '../schemas/authSchemas';
+import * as authControlls from '../controllers/authControllers';
 
 const authRoutes = Router();
 
-authRoutes.post("/signin", authControlls.signInControll);
+authRoutes.post('/signup', validateSchemas(authSchemas.signUp), authControlls.signUpControll);
 
-authRoutes.post("/signup", authControlls.signUpControll);
+authRoutes.post('/signin', authControlls.signInControll);
 
 export default authRoutes;
