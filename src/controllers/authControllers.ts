416938cc731456d70fd8c27ac6dc37eb630928bd
Matheus Melo <matheuscabal821@gmail.com>
2023-01-11 16:17:22 -0300

    📦: auth controll w/ resources & services to work

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,9 +1,22 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
+import * as authServices from '../services/authServices'
 
-export function signInControll(req: Request, res: Response) {
-  return res.send(200);
+export async function signUpControll(req: Request, res: Response) {
+  const inputData = req.body;
+
+  await authServices.signUp(inputData);
+
+  return res.sendStatus(200);
 }
 
-export function signUpControll(req: Request, res: Response) {
-  return res.send(200);
+export async function signInControll(req: Request, res: Response) {
+  const userData = req.body;
+
+  const token = await authServices.signIn(userData);
+
+  return res.status(200).send({
+    status: 'sucesss',
+    time: new Date(),
+    token
+  });
 }
